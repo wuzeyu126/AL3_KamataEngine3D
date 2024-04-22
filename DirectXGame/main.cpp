ba6f01c858feb9f71a873a170410becc77146b7d
wuzeyu126 <wuzeyu126@gmail.com>
2024-04-22 14:40:03 +0900

    01-01_確認課題

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -26,6 +26,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	dxCommon = DirectXCommon::GetInstance();
 	dxCommon->Initialize(win);
 
+
+
 #pragma region 汎用機能初期化
 	// ImGuiの初期化
 	ImGuiManager* imguiManager = ImGuiManager::GetInstance();
